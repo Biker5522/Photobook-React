efbc0b11d10b8f95711c6b72b71942625a53d849
Putixx <80105950+Putixx@users.noreply.github.com>
2022-10-21 14:43:45 +0200

    Add API connection and fix navbar bug

---
+++ frontend/pages/api/CallAPI.ts
@@ -0,0 +1,14 @@
+export async function getUserByUsername (username: any) 
+{
+        try {
+          const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+          const data = await res.json();
+
+          const user = data.find(
+            (u: any) => u.username == username.current?.value
+          );
+          console.log(user);
+        } catch (err) {
+          console.log(err);
+        }
+}
--- frontend/pages/users.tsx
+++ frontend/pages/users.tsx
@@ -1,4 +1,11 @@
+import {useRef} from 'react';
+import { getUserByUsername } from './api/CallAPI';
+
+
 export default function Users() {
+  
+    const username = useRef<any>(null);
+
     return (
       <div className="max-w-2xl mx-auto loginPage">
         <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
@@ -7,23 +14,19 @@ export default function Users() {
               Users
             </h2>
             <div>
-              <label
-                htmlFor="email"
-                className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
-              >
-                User email
-              </label>
               <input
-                type="email"
-                name="email"
-                id="email"
+                ref={username}
+                type="text"
+                name="username"
+                id="username"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="name@company.com"
+                placeholder="Search user by username"
               />
             </div>
             <div className="flex items-center justify-center h-5 ">
               <button
                 type="submit"
+                onClick={getUserByUsername.bind(null, username)}
                 className="w-3/5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Find
